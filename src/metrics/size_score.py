78b8fbad84456e81f1d0be57d115de34148c27b5
romita <romita_p@pursuitsoftware.com>
2025-09-23 12:37:10 -0400

    bus factor

---
+++ src/metrics/size_score.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from typing import Dict, Any
+from ..models import ModelContext, MetricResult
+from ..logging_utils import get_logger
+from ..utils import measure_time
+
+logger = get_logger()
